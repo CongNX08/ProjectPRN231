c454ec6ce7716212f7a8476d759dd595c6eb03e7
hoan111 <hoan1001@gmail.com>
2023-07-23 02:22:49 +0700

    Thêm API đăng kí tài khoản

--- CinemaWebAPI/Controllers/AuthController.cs
+++ CinemaWebAPI/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using BCrypt.Net;
 using BusinessObject.Models;
+using CinemaWebAPI.Config;
 using CinemaWebAPI.Request.Auth;
+using CinemaWebAPI.Response;
 using CinemaWebAPI.Response.Auth;
 using DataAccess.DTO;
 using DataAccess.Repository;
@@ -11,7 +13,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace CinemaWebAPI.Config
+namespace CinemaWebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -45,7 +47,7 @@ namespace CinemaWebAPI.Config
                     var mapper = AutoMapperConfig.InitializeAutomapper<Person, UserDTO>();
                     UserDTO user = mapper.Map<UserDTO>(person);
                     string token = GenerateToken(user);
-                    if (String.IsNullOrEmpty(token))
+                    if (string.IsNullOrEmpty(token))
                     {
                         response.IsSuccess = false;
                         response.ErrorMessages = "Cannot get token because null!";
@@ -69,6 +71,34 @@ namespace CinemaWebAPI.Config
             }
         }
 
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResponse>> Register([FromBody] UserRegisterRequest request)
+        {
+            try
+            {
+                CommonResponse response = new CommonResponse();
+                if (!ModelState.IsValid)
+                {
+                    response.IsSuccess = false;
+                    response.ErrorMessages = "Input không hợp lệ";
+                    return BadRequest(response);
+                }
+                var mapper = AutoMapperConfig.InitializeAutomapper<UserRegisterRequest, Person>();
+                Person person = mapper.Map<Person>(request);
+                person.IsActive = true;
+                person.Type = Constants.UserType.NORMAL_USER;
+                person.Password = BCrypt.Net.BCrypt.HashPassword(request.Password);
+                await repository.CreateAsync(person);
+                response.IsSuccess = true;
+                response.StatusCode = System.Net.HttpStatusCode.OK;
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+        }
+
         private string GenerateToken(UserDTO user)
         {
             var issuer = configuration["Jwt:Issuer"];
@@ -85,7 +115,7 @@ namespace CinemaWebAPI.Config
                 new Claim(JwtRegisteredClaimNames.Jti,
                 Guid.NewGuid().ToString())
              }),
-                Expires = DateTime.UtcNow.AddMinutes(5),
+                Expires = DateTime.UtcNow.AddMinutes(60),
                 Issuer = issuer,
                 Audience = audience,
                 SigningCredentials = new SigningCredentials
---
+++ CinemaWebAPI/Request/Auth/UserRegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaWebAPI.Request.Auth
+{
+    public class UserRegisterRequest
+    {
+        [Required]
+        public string Fullname { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
