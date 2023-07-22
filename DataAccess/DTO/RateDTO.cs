fb05e2c6990d0ae8384ce1efc2721011f9062bcb
hoan111 <hoan1001@gmail.com>
2023-07-22 10:39:33 +0700

    Thêm API lấy danh sách comment

---
+++ DataAccess/DTO/RateDTO.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class RateDTO
+    {
+        public int MovieId { get; set; }
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public string Comment { get; set; }
+        public decimal? NumericRating { get; set; }
+        public string? Time { get; set; }
+    }
+}
